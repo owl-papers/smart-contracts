9a8187ae429031cecc69d79a4a41fa72a53e8985
Pedro Bufulin <pedro.almeida@ufu.br>
2021-11-23 16:00:00 +0000

    contrract assigns random reviewers

--- contracts/ReviewHanlder.sol
+++ contracts/ReviewHanlder.sol
@@ -7,20 +7,26 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 /**
  * @title A contract that handles who will be reviewing a paper
  * @author Pedro Henrique Bufulin de Almeida
- * @notice reviewers register as which category they want to write reviews for. A reviewer
- * deploys this contract. It uses chainlink VRF to handle randomization of selected reviewers.
+ * @notice  A paper author deploys this contract.
+ * It uses chainlink VRF to handle randomization of selected reviewers.
  * Reviewers call the joinAsReviewer function to have a chance to be elegible for a right to review.
  * @custom:experimental This is an experimental contract.
  */
 contract ReviewHandler is VRFConsumerBase, Ownable {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.UintSet;
     bytes32 private sKeyhash;
     uint256 private sFee;
     uint256 public randomValue;
-    address[] public reviewers;
+    address[] public possibleReviewers;
+    address[] public selectedReviewers;
+    EnumerableSet.UintSet private randomIndexes;
+
     ValidateRandom public isvalid;
 
     enum ValidateRandom {
@@ -55,25 +61,42 @@ contract ReviewHandler is VRFConsumerBase, Ownable {
     }
 
     /**
-     * @dev returns an array of n random values between 0 and j.
-     * @param _randomValue The value received from VRF
-     * @param n How many random values to generate
+     * @dev returns a Set of random unique values between 0 and _amount.
+     * @param _amount Maxmium value of random generated values
      */
-    function genMulti(uint256 n, uint256 j)
-        public
-        view
-        returns (uint256[] memory multiRandom)
-    {
+    function genMulti(uint256 _amount) public onlyOwner {
         require(
             isvalid == ValidateRandom.valid,
             "not elegible for random selection yet"
         );
-        multiRandom = new uint256[](n);
-        for (uint256 i = 0; i < n; i++) {
-            multiRandom[i] = (uint256(keccak256(abi.encode(randomValue, i))) %
-                j);
+
+        uint256 i = 0;
+        while (randomIndexes.length() != _amount) {
+            uint256 generated = (uint256(
+                keccak256(abi.encode(randomValue, i))
+            ) % _amount);
+            if (!randomIndexes.contains(generated)) {
+                randomIndexes.add(generated);
+            }
         }
-        return multiRandom;
+    }
+
+    /**
+     * @notice 2-5 reviewers are assgined randomly. The amount of reviewers is 
+     * also a random number from 2 to 5.  
+]    */
+    function assignReviewrs() public onlyOwner {
+        require(
+            possibleReviewers.length >= 5,
+            "cannot call review assignment, not enough reviewers joined"
+        );
+
+        uint256 amountOfReviewers = (randomValue % 4) + 2;
+        genMulti(amountOfReviewers);
+        for (uint256 i = 0; i < randomIndexes.length(); i++) {
+            address selected = possibleReviewers[randomIndexes.at(i)];
+            selectedReviewers.push(selected);
+        }
     }
 
     function getRandomNumber() public returns (bytes32 requestId) {
@@ -89,7 +112,7 @@ contract ReviewHandler is VRFConsumerBase, Ownable {
      * If the sender puts a category that does not exist in enum, it will simply not be selected.
      */
     function joinAsReviewer() public {
-        reviewers.push(msg.sender);
+        possibleReviewers.push(msg.sender);
     }
 
     function setRandomReviewers() public onlyOwner {}
